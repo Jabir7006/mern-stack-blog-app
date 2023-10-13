8913b87db7ed4e07d12d36a050a26dcb18c3b8db
Jabir7006 <www.roman1249@gmail.com>
2023-10-13 17:07:12 +0600

    updated

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,8 @@ app.listen(port, async () => {
   await connectDB();
 });
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: "https://mern-blog-app-447c.onrender.com", credentials: true }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static(path.join(__dirname, "public")));
