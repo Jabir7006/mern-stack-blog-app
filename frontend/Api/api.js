bf13c27cef57bf28d493362bc7e2f9ac7b6a9c51
Jabir7006 <www.roman1249@gmail.com>
2024-02-22 13:07:06 +0600

    update api.js

--- frontend/Api/api.js
+++ frontend/Api/api.js
@@ -1,2 +1,2 @@
-// export const baseURL = "https://mern-blog-app-backend-0673.onrender.com";
-export const baseURL = "http://localhost:3000";
+export const baseURL = "https://mern-blog-app-backend-0673.onrender.com";
+// export const baseURL = "http://localhost:3000";
